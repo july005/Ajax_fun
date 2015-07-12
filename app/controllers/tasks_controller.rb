ba36014679d676f45d871a6665c5092a4270f8c3
ErinUC <erinuc@gmail.com>
2015-07-12 09:53:02 -0400

    Added before action filter to create the @tasks instance

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,49 +1,25 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :all_tasks, only: [:index, :create]
+  respond_to :html, :js
   
-  def index
-    @tasks = Task.all
-  end
+  # index action has been removed
 
   def new
     @task = Task.new
   end
 
   def create
-    @task = Task.new(task_params)
-
-    if @task.save
-      flash[:notice] = 'Task created'
-      redirect_to @task
-    else
-      flash[:alert] = 'Task not created'
-      render :new
-    end
-  end
-
-  def update
-    if @task.update_attributes(task_params)
-      flash[:notice] = 'Task updated'
-      redirect_to @task
-    else
-      flash[:alert] = 'Task not updated'
-      render :edit
-    end      
-  end
-
-  def destroy
-    @task.destroy
-    flash[:notice] = 'Task deleted'
-    redirect_to @task
+    @task = Task.create(task_params)
   end
 
   private
 
-    def set_task
-      @task = Task.find(params[:id])
-    end
+  def all_tasks
+    @tasks = Task.all
+  end
 
-    def task_params
-      params.require(:task).permit(:description, :deadline)
-    end
+  def task_params
+    params.require(:task).permit(:description, :deadline)
+  end
+
 end
